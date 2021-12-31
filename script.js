3773ee6a126a0c6d148501ae3a449afd05f6f92f
Laura Gupta <95309002+lauragupta@users.noreply.github.com>
2021-12-31 00:24:02 -0600

    add js file and comment planned framework of steps and connect js to html

---
+++ script.js
@@ -0,0 +1,71 @@
+/*set variable for timer
+
+/* click starts the quiz and quiz timer
+
+/*first question posts
+
+/* correct answer continues
+/*incorrect answer deducts time and continues 
+
+
+/*second question posts
+
+/* correct answer continues
+/*incorrect answer deducts time and continues 
+
+
+/*third question posts
+
+/* correct answer continues
+/*incorrect answer deducts time and continues 
+
+
+/*fourth question posts
+
+/* correct answer continues
+/*incorrect answer deducts time and continues 
+
+
+/*fifth question posts
+
+/* correct answer continues
+/*incorrect answer deducts time and continues 
+
+
+/*sixth question posts
+
+/* correct answer continues
+/*incorrect answer deducts time and continues 
+
+
+/*seventh question posts
+
+/* correct answer continues
+/*incorrect answer deducts time and continues 
+
+
+/*eighth question posts
+
+/* correct answer continues
+/*incorrect answer deducts time and continues 
+
+/*ninth question posts
+
+/* correct answer continues
+/*incorrect answer deducts time and continues 
+
+
+/*tenth question posts
+
+/* correct answer continues
+/*incorrect answer deducts time and continues 
+
+/* timer goes off alert
+
+/* configure score combo of time left and correct answers
+
+/* show score
+
+/* offer to add initials with score to save in local storage
+
+/* offer to play again to try to beat your score
