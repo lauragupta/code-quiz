85330eb024033a7ed0f1c7b78944bfec0ab2db7e
Laura Gupta <95309002+lauragupta@users.noreply.github.com>
2022-01-01 13:14:16 -0600

    enlarge main box to accomidate text, add id's for timer function, get timer function working (though not on click yet)

--- script.js
+++ script.js
@@ -1,71 +1,60 @@
-/*set variable for timer
+/* set variable for timer */ 
+/*var countdownTimer = "Time remaining : " + timer; */
+var timeRemaining = document.getElementById("quizTimer");
+/* set variable for score */ 
+var currentScore = document.getElementById("quizScore");
 
-/* click starts the quiz and quiz timer
+var countdownTimer = 15
 
-/*first question posts
-
-/* correct answer continues
-/*incorrect answer deducts time and continues 
-
-
-/*second question posts
-
-/* correct answer continues
-/*incorrect answer deducts time and continues 
-
-
-/*third question posts
-
-/* correct answer continues
-/*incorrect answer deducts time and continues 
-
-
-/*fourth question posts
-
-/* correct answer continues
-/*incorrect answer deducts time and continues 
+/* click starts the quiz and quiz timer */ 
+document.getElementById("start-button").addEventListener("click", timer)
+/* timer function */
+    var timer = setInterval(function () {
+            timeRemaining.textContent = "Time remaining: " + countdownTimer;
+            countdownTimer --;
+            if(countdownTimer === 0) {
+                clearInterval(timer);
+                timeRemaining.textContent = ""
+        }
+    }, 1000);
 
 
-/*fifth question posts
+/* first question posts */ 
 
-/* correct answer continues
-/*incorrect answer deducts time and continues 
-
-
-/*sixth question posts
-
-/* correct answer continues
-/*incorrect answer deducts time and continues 
+/* correct answer  and points and continues */ 
+/* incorrect answer deducts time and continues */ 
 
 
-/*seventh question posts
+/*second question posts */ 
 
-/* correct answer continues
-/*incorrect answer deducts time and continues 
-
-
-/*eighth question posts
-
-/* correct answer continues
-/*incorrect answer deducts time and continues 
-
-/*ninth question posts
-
-/* correct answer continues
-/*incorrect answer deducts time and continues 
+/* correct answer  and points and continues */ 
+/*incorrect answer deducts time and continues */ 
 
 
-/*tenth question posts
+/*third question posts */ 
 
-/* correct answer continues
-/*incorrect answer deducts time and continues 
+/* correct answer  and points and continues */ 
+/*incorrect answer deducts time and continues */ 
 
-/* timer goes off alert
 
-/* configure score combo of time left and correct answers
+/*fourth question posts */ 
 
-/* show score
+/* correct answer  and points and continues */
+/*incorrect answer deducts time and continues */
 
-/* offer to add initials with score to save in local storage
 
-/* offer to play again to try to beat your score
+/*fifth question posts */
+
+/* correct answer  and points and continues */
+/*incorrect answer deducts time and continues */
+
+
+/* timer goes off alert */
+
+/* configure score combo of time left and correct answers */
+
+/* show score */
+
+/* offer to add initials with score to save in local storage */
+
+/* offer to play again to try to beat your score */
