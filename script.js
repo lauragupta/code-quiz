ba6f2fe8b9590b1f65eb79079bb0a3be30e1d656
Laura Gupta <95309002+lauragupta@users.noreply.github.com>
2022-01-03 22:57:32 -0800

    change plan for posting question, make array of questions, make load question function, load first question.

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
 /* set variable for timer */ 
-/*var countdownTimer = "Time remaining : " + timer; */
 var timeRemaining = document.getElementById("quizTimer");
 /* set variable for score */ 
-var currentScore = document.getElementById("quizScore");
+var quizScore = document.getElementById("quizScore");
+var quizQuestion = document.getElementById("quizQuestion");
+var optionA = document.getElementById("answerA");
+
+var optionB = document.getElementById("answerB");
+
+var optionC = document.getElementById("answerC");
+
+var optionD = document.getElementById("answerD");
 
 var countdownTimer = 10;
 
 var score = 0;
 
+/*Create an Array of Questions*/
+var questionArray = [
+    "What part of the html document does the title go?",
+    "What is the shorthand order of directions for a margin in CSS?",
+    "What symbols are used around comments in html?",
+    "Which of the following heading is the biggest by default?"
+]
+
+
 /* timer function */
 function onClick() {
     timeRemaining.textContent = "Time remaining: " + countdownTimer;
-    currentScore.textContent = "Score: " + score;
+    quizScore.textContent = "Score: " + score;
+    loadQuestions();
     var startButton = document.getElementById("start-button");
-    startButton.setAttribute("style", "display:none")
+    startButton.setAttribute("style", "display:none");
     var timer = setInterval(function () {
         countdownTimer --;
         timeRemaining.textContent = "Time remaining: " + countdownTimer;
@@ -28,7 +45,23 @@ function onClick() {
 document.getElementById("start-button").addEventListener("click", onClick);
 
 
-/* first question posts */ 
+//add event llistener for answer buttons child of new div
+
+//answerButtons.addEventListener("click", function(nextQuestion))
+
+/* Post first question*/ 
+function loadQuestions() {
+    var question = 0;
+    if(question === questionArray.length) {
+        return;
+    } else {
+        quizQuestion.textContent = questionArray[question];
+    }
+    
+}
+
+/*next question function*/
+
 
 /* correct answer verify, add points and continues */ 
 /* incorrect answer deducts time and continues */ 
